a7ab3aa5afdcda4846c502a0c950c12e132222c2
GrGLeo <graziani.gothe.leo@gmail.com>
2024-11-03 17:35:36 +0000

    / handler 0

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+  "fmt"
 	"net/http"
 )
 
 func main() {
   mux := http.NewServeMux()
+
+  mux.Handle("/", http.HandlerFunc(homepage))
   server := &http.Server {
     Addr: ":8080",
     Handler: mux,
   }
   if err := server.ListenAndServe(); err != nil {
   }
+}
+
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintln(w, "Welcome to the homepage!")
 }
