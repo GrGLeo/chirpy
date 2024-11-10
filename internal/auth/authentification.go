1555d252f7e734a7d19f7b056a60ad96189d57f5
GrGLeo <graziani.gothe.leo@gmail.com>
2024-11-10 15:49:30 +0100

    working on jwt

--- internal/auth/authentification.go
+++ internal/auth/authentification.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -21,4 +24,41 @@ func CheckPasswordHash(pwd, hash string) error {
     return err
   }
   return nil
+}
+
+func MakeJWT(userId uuid.UUID, tokenSecret string, expiredIn time.Duration) (string, error) {
+  time := time.Now()
+  
+  claims := &jwt.RegisteredClaims{
+    Issuer: "chirpy",
+    IssuedAt: jwt.NewNumericDate(time),
+    ExpiresAt: jwt.NewNumericDate(time.Add(expiredIn)),
+    Subject: userId.String(),
+  }
+  
+  token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+  return token.SignedString(tokenSecret)
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+  type CustomClaims struct {
+    jwt.RegisteredClaims
+  }
+
+  token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+    return []byte(tokenString), nil
+  })
+  
+  if err != nil {
+    return uuid.Nil, err
+  }
+
+  if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+    userID, err := uuid.Parse(claims.Subject)
+    if err != nil {
+      return uuid.Nil, err
+    }
+    return userID, nil
+  }
+  return uuid.Nil, fmt.Errorf("invalid token")
 }
