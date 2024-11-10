55e33de429917f91eb982e26e6211eb148095bc9
GrGLeo <graziani.gothe.leo@gmail.com>
2024-11-10 13:38:39 +0100

    rewrite create users to add pwd

--- auth.go
+++ auth.go
@@ -1,6 +1,67 @@
 package main
 
-// import (
-//   "github.com/GrGLeo/chirpy/internal/auth"
-// )
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
 
+	"github.com/google/uuid"
+
+	"github.com/GrGLeo/chirpy/internal/auth"
+	"github.com/GrGLeo/chirpy/internal/database"
+)
+
+func (cfg *apiConfig) CreateUser(w http.ResponseWriter, r *http.Request) {
+    type RequestBody struct {
+      Password string `json:"password"`
+      Email string `json:"email"` 
+  }
+  
+  type User struct {
+    ID        uuid.UUID `json:"id"`
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+    Email     string    `json:"email"`
+  }
+
+  var reqBody RequestBody
+  if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+    http.Error(w, "Invalid request body", http.StatusBadRequest)
+    return
+  }
+  
+  hashedPassword, err := auth.HashPassword(reqBody.Password)
+  if err != nil {
+    http.Error(w, "Internal server error", http.StatusInternalServerError)
+  }
+
+  userParam := database.CreateUserParams{
+    Email: reqBody.Email,
+    HashedPassword: hashedPassword,
+  }
+
+  newUser, err := cfg.dbQueries.CreateUser(r.Context(), userParam)
+  user := User{
+    ID: newUser.ID,
+    CreatedAt: newUser.CreatedAt,
+    UpdatedAt: newUser.UpdatedAt,
+    Email: newUser.Email,
+  }
+  
+  if err != nil {
+    http.Error(w, "Error while creating user", http.StatusConflict)
+    return
+  }
+
+  data, err := json.Marshal(user)
+  if err != nil {
+    log.Printf("Error marshaling json: %s", err)
+    w.WriteHeader(500)
+    return
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(http.StatusCreated)
+  w.Write(data)
+}
--- main.go
+++ main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"sync/atomic"
-	"time"
-
-	"github.com/google/uuid"
 
 	"github.com/GrGLeo/chirpy/internal/database"
 	"github.com/joho/godotenv"
@@ -51,49 +46,6 @@ func (cfg *apiConfig) reset(w http.ResponseWriter, r * http.Request) {
   w.Write([]byte("Success"))
 }
 
-
-func (cfg *apiConfig) CreateUser(w http.ResponseWriter, r *http.Request) {
-  type RequestBody struct {
-    Email string `json:"email"` 
-  }
-
-  type User struct {
-    ID        uuid.UUID `json:"id"`
-    CreatedAt time.Time `json:"created_at"`
-    UpdatedAt time.Time `json:"updated_at"`
-    Email     string    `json:"email"`
-  }
-
-  var reqBody RequestBody
-  if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-    http.Error(w, "Invalid request body", http.StatusBadRequest)
-    return
-  }
-  
-  newUser, err := cfg.dbQueries.CreateUser(r.Context(), reqBody.Email)
-  user := User{
-    ID: newUser.ID,
-    CreatedAt: newUser.CreatedAt,
-    UpdatedAt: newUser.UpdatedAt,
-    Email: newUser.Email,
-  }
-  
-  if err != nil {
-    http.Error(w, "Error while creating user", http.StatusConflict)
-    return
-  }
-
-  data, err := json.Marshal(user)
-  if err != nil {
-    log.Printf("Error marshaling json: %s", err)
-    w.WriteHeader(500)
-    return
-  }
-
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(http.StatusCreated)
-  w.Write(data)
-}
 
 func (cfg *apiConfig) metrics (w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html")
